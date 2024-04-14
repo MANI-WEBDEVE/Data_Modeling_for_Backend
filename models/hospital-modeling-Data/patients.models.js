1e842f25c2abbc7939c1a65659e57d51fd7b2405
MANI-WEBDEVE <nudmaufon@gmail.com>
2024-04-13 21:27:13 -0400

    few-changes

--- models/hospital-modeling-Data/patients.models.js
+++ models/hospital-modeling-Data/patients.models.js
@@ -1,6 +1,33 @@
 import mongoose from "mongoose";
 
-const patientsSchema = new mongoose.Schema({}, {timestamps:true})
+const patientsSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required: true,
+    },
+    diagonsedWith:{
+        type:String,
+        required:true
+    },
+    addreas:{
+        type:String,
+        required:true
+    },
+    age:{
+        type:String,
+        required:true
+    },
+    bloodGroup:{
+        type:String,
+        enum:['A','B','AB','O'],
+        required:true
+    },
+    gender:{
+        type:String,
+        enum:["Male", "Female", "Shemale","Other"]
+    }
+
+}, {timestamps:true})
 
 
 export const Patients = mongoose.model('patients', patientsSchema)
