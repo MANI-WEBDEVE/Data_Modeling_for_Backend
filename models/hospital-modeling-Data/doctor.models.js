b904eaec8d2c0d9d64ecfa183fd74ad967d833f7
MANI-WEBDEVE <nudmaufon@gmail.com>
2024-04-15 11:52:41 -0400

    few chamges

--- models/hospital-modeling-Data/doctor.models.js
+++ models/hospital-modeling-Data/doctor.models.js
@@ -1,6 +1,30 @@
 import mongoose from "mongoose";
 
-const doctorSchema = new mongoose.Schema({}, {timestamps:true})
+const doctorSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    salary:{
+        type:String,
+        required: true
+    },
+    qualification:{
+        type:String,
+        required:true
+    },
+    experienceInYear:{
+        type:Number,
+        default:0
+    },
+    worksInHospitals:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Hospital",
+            default:0
+        }
+    ]
+}, {timestamps:true})
         
 
 export const Doctor = mongoose.model('Doctor', doctorSchema) 
